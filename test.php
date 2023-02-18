1fa300a5d94407e6f1a6529e793eb91cbb7d492c
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 01:24:17 +0200

    step 3

--- test.php
+++ test.php
@@ -1,6 +1,11 @@
-<?php 
-    "I am getting errors but I can't see the errors"    
+<?php
+$name = "mahmoud";
+$email = "mahmud.elali98@gmail.com";
+
+echo "My name is " . $name . " and my email is " . $email;
+print("</br>");
+$height = 187;
+$width = 2;
+$area = $height * $width;
+echo $area;
 ?>
-
-
-
