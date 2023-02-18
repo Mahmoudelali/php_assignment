0121e1787b61870a3ed5b907e152d151280de522
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 01:38:47 +0200

    step 5

--- test.php
+++ test.php
@@ -1,18 +1,10 @@
 <?php
-$name = "mahmoud";
-$position = "mentee";
-$github_url = "https://github.com/Mahmoudelali/php_assignment/";
-echo "Hello, I'm " . $name . " I'm a " . $position . "please check my github link" . $github_url;
-print("</br>");
-echo "Hello, I'm $name, I'm a $position please check my github link $github_url"
+$course_name = "backend";
+$enrolled_students = 300;
+$price = 100.33;
+$on_discount = true;
+echo "details </br>$course_name : backend </br>enrolled_students  : $enrolled_students </br> price :$price </br> on_discount $on_discount"
+
+
 
     ?>
-<!-- 
-Create a 3 variables, name, position & github_url and assign them there with respective values.
-Output the 3 variables using the 3 methods below, the output should be similar to this one 
-Hello, I'm Gaby, I'm a tech consultant please check my github link https://github.com/gk-git.
-Using the double quotation " and the .
-Using the double quotation " and without using the .
-Without using the double quotation ".
-(Optional) try to output the previous without using the . and without using the previous answer
- from any of the previous solutions -->
