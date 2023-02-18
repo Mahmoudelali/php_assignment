8d369ec3f272f04cf3ba08bd6352668864755ca6
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 01:31:14 +0200

    step 4

--- test.php
+++ test.php
@@ -1,11 +1,18 @@
 <?php
 $name = "mahmoud";
-$email = "mahmud.elali98@gmail.com";
-
-echo "My name is " . $name . " and my email is " . $email;
+$position = "mentee";
+$github_url = "https://github.com/Mahmoudelali/php_assignment/";
+echo "Hello, I'm " . $name . " I'm a " . $position . "please check my github link" . $github_url;
 print("</br>");
-$height = 187;
-$width = 2;
-$area = $height * $width;
-echo $area;
-?>
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url"
+
+    ?>
+<!-- 
+Create a 3 variables, name, position & github_url and assign them there with respective values.
+Output the 3 variables using the 3 methods below, the output should be similar to this one 
+Hello, I'm Gaby, I'm a tech consultant please check my github link https://github.com/gk-git.
+Using the double quotation " and the .
+Using the double quotation " and without using the .
+Without using the double quotation ".
+(Optional) try to output the previous without using the . and without using the previous answer
+ from any of the previous solutions -->
