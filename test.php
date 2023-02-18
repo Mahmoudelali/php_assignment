3f65662d437d8842fbe6b024557fcdcae7e78901
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 01:42:28 +0200

    step 6

--- test.php
+++ test.php
@@ -1,9 +1,10 @@
 <?php
-$course_name = "backend";
-$enrolled_students = 300;
-$price = 100.33;
-$on_discount = true;
-echo "details </br>$course_name : backend </br>enrolled_students  : $enrolled_students </br> price :$price </br> on_discount $on_discount"
+function calculateArea($height, $width)
+{
+    return $height * $width;
+}
+echo calculateArea(6, 3)
+
 
 
 
