2b32d730176a302e632629906a8b677a4d3f8ec1
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 21:11:03 +0200

    step 2

--- basics 2/basics_2.php
+++ basics 2/basics_2.php
@@ -1,3 +1,14 @@
 <?php
-echo "Tomorrow I 'll learn PHP global variables. </br> This is a bad command: sudo rm -rf ";
-?>
+// Write a PHP function to test whether a number is greater than 30, 20 or 10
+function checkNumber($number)
+{
+    if ($number > 30) {
+        return "number is greater than 30";
+    } elseif ($number > 20) {
+        return "number is greater than 20";
+    } elseif ($number > 10) {
+        return "number is greater than 10";
+    }
+}
+echo checkNumber(11)
+    ?>
