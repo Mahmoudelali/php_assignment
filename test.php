d7211da578a4252c35b80423b36e05892a765205
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 20:47:44 +0200

    step 9

--- test.php
+++ test.php
@@ -1,8 +1,6 @@
 <?php
 
-$multiDimentionalArr = ["Eggs" => ["balade", "mazere3"], "Milk" => ['Fresh', "Taanayel"], "Water Pack" => ['Tanoureen', 'Reem'],];
-
-
-echo "Hey Sir Please I need 1 pack of " . $multiDimentionalArr["Eggs"][0] . " eggs and 3 " . $multiDimentionalArr["Water Pack"][1] . " Water Pack "
+$myName = "Mahmoud Elali" ;
+var_dump($myName);
 
     ?>
