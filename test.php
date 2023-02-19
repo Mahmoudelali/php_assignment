2012f945ce768c1e63199ba09e3b99e99b50ad3d
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 02:00:15 +0200

    step 7

--- test.php
+++ test.php
@@ -1,11 +1,17 @@
 <?php
-function calculateArea($height, $width)
-{
-    return $height * $width;
+
+$birth_year = 1998;
+$current_year = 2023;
+
+
+if ($current_year - $birth_year > 18) {
+    echo "You can drive";
+} else {
+    echo "You still a kid, go and play GTA";
 }
-echo calculateArea(6, 3)
 
 
 
+?>
 
-    ?>
+
