e7a73c9d0e0e0485d0acc19c74bdeec93444ccd3
mahmoudElali <mahmud.elali98@gmail.com>
2023-02-19 20:25:21 +0200

    step 8-a

--- test.php
+++ test.php
@@ -1,17 +1,13 @@
 <?php
 
-$birth_year = 1998;
-$current_year = 2023;
-
-
-if ($current_year - $birth_year > 18) {
-    echo "You can drive";
-} else {
-    echo "You still a kid, go and play GTA";
+$indexedArr = ["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+$newArr = [];
+for ($i = 0; $i < count($indexedArr); $i++) {
+    if ($indexedArr[$i] == "Eggs" or $indexedArr[$i] == "Milk" or $indexedArr[$i] == "Cheese" or $indexedArr[$i] == "Watermelon") {
+        array_push($newArr, $indexedArr[$i]);
+    }
 }
+echo " Hello Sir, do you have $newArr[0], $newArr[1] ,
+and $newArr[2]? Also if you sell fruits can I find a $newArr[3]"
 
-
-
-?>
-
-
+    ?>
